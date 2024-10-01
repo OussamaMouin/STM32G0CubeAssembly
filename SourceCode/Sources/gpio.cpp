0c506e9fa8b2c8ab7acd0544bc8b284f4ef414e4
FIRMach16 <123565464+FIRMach16@users.noreply.github.com>
2024-10-01 10:28:00 +0200

    Add files via upload

---
+++ SourceCode/Sources/gpio.cpp
@@ -0,0 +1,14 @@
+#include "Headers/gpio.h"
+#include "ui_gpio.h"
+
+GPIO::GPIO(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::GPIO)
+{
+    ui->setupUi(this);
+}
+
+GPIO::~GPIO()
+{
+    delete ui;
+}
